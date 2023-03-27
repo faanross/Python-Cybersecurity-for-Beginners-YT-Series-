6c7e659f99689e8e36c800141bc6203cbceccea3
Faan Ross <moi@faanross.com>
2023-03-27 17:41:44 -0400

    firewall simulator

--- 01_firewall_simulator.py
+++ 01_firewall_simulator.py
@@ -1,1 +1,9 @@
 import random
+
+def generate_random_ip():
+    """Generate a random IP address."""
+    return f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
+
+def check_firewall_rules(ip, rules):
+    """Check if the IP address matches any firewall rule and return the action."""
+    for rule_ip, action in rules.items():
