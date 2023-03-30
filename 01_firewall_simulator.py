faa4259013ba28c807a5a1b0c5ab11c9a19eba54
Faan Ross <moi@faanross.com>
2023-03-30 12:03:18 -0400

    dos_blocker

--- 01_firewall_simulator.py
+++ 01_firewall_simulator.py
@@ -17,14 +17,17 @@ def main():
         "192.168.1.1": "block",
         "192.168.1.4": "block",
         "192.168.1.9": "block",
-        "192.168.1.13": "block"
+        "192.168.1.13": "block",
+        "192.168.1.16": "block",
+        "192.168.1.19": "block"
     }
 
       # Simulate network traffic
-    for _ in range(10):
-        ip_address = generate_random_ip()
-        action = check_firewall_rules(ip_address, firewall_rules)
-        print(f"IP: {ip_address}, Action: {action}")
+    for _ in range(12):
+     ip_address = generate_random_ip()
+     action = check_firewall_rules(ip_address, firewall_rules)
+     random_number = random.randint(0, 9999)
+     print(f"IP: {ip_address}, Action: {action}, Random: {random_number}")
 
 if __name__ == "__main__":
     main()
