4f2eb57fcabb68de4a6ee0cf4bda8b386d38046d
Faan Ross <moi@faanross.com>
2023-03-30 12:04:27 -0400

    dos_blocker

--- 02_dos_blocker.py
+++ 02_dos_blocker.py
@@ -9,3 +9,12 @@ THRESHOLD = 100
 def packet_callback(packet):
     src_ip = packet[IP].src
     packet_count[src_ip] += 1
+
+      current_time = time.time()
+    time_interval = current_time - start_time
+    if time_interval >= 1:
+        for ip, count in packet_count.items():
+            packet_rate = count / time_interval
+            if packet_rate > THRESHOLD:
+                print(f"Blocking IP: {ip}, packet rate: {packet_rate}")
+                os.system(f"iptables -A INPUT -s {ip} -j DROP")
