18e1aedd96812ea359a450de955ff316357ae2a9
Faan Ross <moi@faanross.com>
2023-03-31 10:56:01 -0400

    dos blocker test

--- 02_dos_blocker_tester.py
+++ 02_dos_blocker_tester.py
@@ -2,22 +2,18 @@ import sys
 import time
 from scapy.all import Ether, IP, UDP, sendp
 
-TARGET_IP = "192.168.1.X"  # Replace with the target IP address
-INTERFACE = "eth0"  # Replace with your network interface
+TARGET_IP = "192.168.2.20"  # Replace with the target IP address
+INTERFACE = "Ethernet"  # Replace with your network interface
 
 def send_packets(target_ip, interface):
     packet = Ether() / IP(dst=target_ip) / UDP()
-    start_time = time.time()
 
     while True:
-        sendp(packet, iface=interface)
-        current_time = time.time()
-        time_interval = current_time - start_time
-
-        if time_interval >= 1:
-            break
+        sendp(packet, iface=interface, loop=1, inter=0.005)
 
 if __name__ == "__main__":
     if sys.version_info[0] < 3:
         print("This script requires Python 3.")
         sys.exit(1)
+
+    send_packets(TARGET_IP, INTERFACE)
