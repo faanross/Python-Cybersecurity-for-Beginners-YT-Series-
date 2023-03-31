1da1573d23317d823b35489af747c1cfb819ae83
Faan Ross <moi@faanross.com>
2023-03-31 10:21:46 -0400

    dos blocker test

--- 02_dos_blocker_tester.py
+++ 02_dos_blocker_tester.py
@@ -1,3 +1,6 @@
 import sys
 import time
 from scapy.all import Ether, IP, UDP, sendp
+
+TARGET_IP = "192.168.1.X"  # Replace with the target IP address
+INTERFACE = "eth0"  # Replace with your network interface
