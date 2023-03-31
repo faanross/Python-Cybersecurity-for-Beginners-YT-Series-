35da8dd6c168b04b1f7377de4440d95608e1bdc1
Faan Ross <moi@faanross.com>
2023-03-31 10:22:01 -0400

    dos blocker test

--- 02_dos_blocker_tester.py
+++ 02_dos_blocker_tester.py
@@ -4,3 +4,7 @@ from scapy.all import Ether, IP, UDP, sendp
 
 TARGET_IP = "192.168.1.X"  # Replace with the target IP address
 INTERFACE = "eth0"  # Replace with your network interface
+
+def send_packets(target_ip, interface):
+    packet = Ether() / IP(dst=target_ip) / UDP()
+    start_time = time.time()
