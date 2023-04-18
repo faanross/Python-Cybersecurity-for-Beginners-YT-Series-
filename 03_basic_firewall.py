e550b2e1e51a42fb04fa43b946758e40d66ee69b
Faan Ross <moi@faanross.com>
2023-04-18 08:16:38 -0400

    firewall for lesson 3

--- 03_basic_firewall.py
+++ 03_basic_firewall.py
@@ -12,3 +12,10 @@ def read_ip_file(filename):
     with open(filename, "r") as file:
         ips = [line.strip() for line in file]
     return set(ips)
+
+# Check for Nimda worm signature
+def is_nimda_worm(packet):
+    if packet.haslayer(TCP) and packet[TCP].dport == 80:
+        payload = packet[TCP].payload
+        return "GET /scripts/root.exe" in str(payload)
+    return False
