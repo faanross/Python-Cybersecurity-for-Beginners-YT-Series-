60eb786c8aa1b3e81236c5c66c02c1edbb5d3e96
Faan Ross <moi@faanross.com>
2023-04-18 08:16:26 -0400

    firewall for lesson 3

--- 03_basic_firewall.py
+++ 03_basic_firewall.py
@@ -3,3 +3,12 @@ import sys
 import time
 from collections import defaultdict
 from scapy.all import sniff, IP, TCP
+
+THRESHOLD = 40
+print(f"THRESHOLD: {THRESHOLD}")
+
+# Read IPs from a file
+def read_ip_file(filename):
+    with open(filename, "r") as file:
+        ips = [line.strip() for line in file]
+    return set(ips)
